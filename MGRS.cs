67c6e54d472a7cbbd3bbdaa5d313e8f18d0d1f2d
Liam Townsley <contact@liamtownsley.me>
2025-05-14 16:42:31 +0100

    refactor: cleanup project

---
+++ MGRS.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    partial class MGRS()
+    {
+        public Dictionary<string, string> ParseMGRS(string input)
+        {
+            var match = MGRS_REGEX().Match(input);
+            if (match.Success)
+            {
+                var GZD = match.Groups[1].Value; // Grid Zone Designator (45V)
+                var UC = match.Groups[2].Value; // 100,000m square identifier (UC) 
+                var Easting = match.Groups[3].Value; // Easting (76232)
+                var Northing = match.Groups[4].Value; // Northing (63380)
+
+                var result = new Dictionary<string, string>
+            {
+                { "coordinate", $"{Easting} {Northing}" },
+                { "additional", $"{GZD} {UC}" }
+            };
+
+                return result;
+            }
+            else
+            {
+                throw new Exception("Invalid MGRS Input");
+            }
+        }
+
+
+        [GeneratedRegex(@"^(\d{2}[A-Z])?\s?([A-Z]{2})?\s?(\d{5})\s(\d{5})$")]
+        private static partial Regex MGRS_REGEX(); // Military Grid Reference System
+    }
+}
---
+++ Sarneg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+partial class Sarneg()
+{
+    public string Encode(string input, string key)
+    {
+        Dictionary<char, char> encodeMap = key
+        .Select((ch, i) => new { ch, i })
+        .ToDictionary(x => (char)('0' + x.i), x => x.ch);
+        var output = new string(input.Select(ch => encodeMap.TryGetValue(ch, out var decoded) ? decoded : ch).ToArray());
+        return Regex.Replace(output, @"[^A-Z\s]", "", RegexOptions.IgnoreCase);
+    }
+
+    public string GetValidChallengeWord()
+    {
+        string? challengeWord;
+        do
+        {
+            Console.Write("Enter SARNEG Challenge Word: ");
+            challengeWord = Console.ReadLine();
+            if (!IsValidSarneg(challengeWord))
+            {
+                Console.WriteLine("Invalid SARNEG Challenge Code - Please try again.");
+            }
+        } while (challengeWord == null || !IsValidSarneg(challengeWord));
+
+        return challengeWord;
+    }
+
+    public bool IsValidSarneg(string? challengeWord)
+    {
+        return SARNEG_REGEX().IsMatch(challengeWord ?? "") && !string.IsNullOrWhiteSpace(challengeWord) && challengeWord.Distinct().Count() == challengeWord.Length;
+    }
+
+    [GeneratedRegex(@"^[A-Z]{10}$")]
+    private static partial Regex SARNEG_REGEX();
+}
